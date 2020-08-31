c5ed94995652e14316908d1d3d57c1068c97d4a2
Hala Alkhellow <halakhellow1996@gmail.com>
2020-08-31 23:27:20 +0300

    add Coin component

---
+++ src/Coin.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Coin.css";
+
+class Coin extends Component {
+  render() {
+    return <img className="Coin" src={this.props.src} alt={this.props.side} />;
+  }
+}
+
+export default Coin;
