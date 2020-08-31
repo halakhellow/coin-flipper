9c30cef1bb000f22a1ab42fdfd0d6740330954d1
Hala Alkhellow <halakhellow1996@gmail.com>
2020-08-31 23:36:02 +0300

    Updates

---
+++ precache-manifest.71e0ec2d02ddfdfdc471c0499e11f7c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a948dea1b5bd294cdbbfea750f586ff",
+    "url": "/coin-flipper/index.html"
+  },
+  {
+    "revision": "3cc5e6383a4bcb39d111",
+    "url": "/coin-flipper/static/css/main.809da9c7.chunk.css"
+  },
+  {
+    "revision": "ad1a01936bb887064e7f",
+    "url": "/coin-flipper/static/js/2.816af10f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin-flipper/static/js/2.816af10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc5e6383a4bcb39d111",
+    "url": "/coin-flipper/static/js/main.c3018d7d.chunk.js"
+  },
+  {
+    "revision": "c0b739701cea77232e22",
+    "url": "/coin-flipper/static/js/runtime-main.43faeb80.js"
+  },
+  {
+    "revision": "682bae0d4d035b9f812be55640c59996",
+    "url": "/coin-flipper/static/media/head.682bae0d.jpg"
+  },
+  {
+    "revision": "2f7c719465c715e3bfaf80afb4c77fdb",
+    "url": "/coin-flipper/static/media/tail.2f7c7194.jpg"
+  }
+]);
